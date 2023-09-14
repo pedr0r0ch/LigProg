20e0bc4d5d26765a80d1bbcaac1c54cfd6cca523
Pedro Rocha <pedrorocha@poli.ufrj.br>
2023-09-14 01:04:00 -0300

    new implementations in exibirAlunoMaiorMedia() method

--- classeGestorAcademico.cpp
+++ classeGestorAcademico.cpp
@@ -6,3 +6,39 @@ GestorAcademico :: GestorAcademico (tipoSemestre periodoVigenteConstrutor, vecto
     vetorAlunos = vectorAlunosConstrutor;
     periodoVigente = periodoVigenteConstrutor;
 };
+
+void GestorAcademico :: evolucaoNotasFinais(){
+    for()
+};
+
+void GestorAcademico :: agruparDisciplinasAdm(){
+
+};
+
+void GestorAcademico :: agruparDisciplinasAlunos(){
+
+};
+
+void GestorAcademico :: exibirDisciplinaMaiorMedia(){
+
+};
+
+void GestorAcademico :: exibirDisciplinaMaiorEvolucao(){
+
+};
+
+void GestorAcademico :: exibirAlunoMaiorMedia(){
+    unsigned short  indice,
+                    indiceMaiorMedia,
+                    maiorMedia = 0;
+
+    for(indice = 0; indice < vetorAlunos.size(); indice++){
+        if (vetorAlunos[indice].getMedia > maiorMedia){
+            maiorMedia = vetorAlunos[indice].getMedia;
+            indiceMaiorMedia = indice;
+        }
+    }
+    Print( "O aluno que possui a maior media eh o aluno:",  vetorAlunos[indiceMaiorMedia].nomeAluno,
+           "Com media equivalente de: ", vetorAlunos[indiceMaiorMedia].getMedia, ".");
+
+};
